1382fb6b69b8e1e3baa72475c61f727e349bc19e
afsar-uddin <afsarwebdev@gmail.com>
2021-10-31 10:32:26 +0600

    port change

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-/*
-europTour
-jOVs9sdKvzAchg2F
-*/
-
 const express = require('express');
+
 const { MongoClient } = require('mongodb');
+
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 require('dotenv').config();
 
-// middleware
+// MIDDLEWARE
 app.use(cors())
 app.use(express.json())
 
-// DB connection
+// DB CONNECTION
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cmhhb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -33,32 +30,35 @@ async function europeTour() {
         const tripTypesCollection = database.collection("tripTypes");
         const tripCollection = database.collection("trips");
 
-        // GET API
+        // GET BANNER API
         app.get('/banner', async (req, res) => {
             const cursor = bannerCollection.find({});
             const bannerArray = await cursor.toArray();
             res.json(bannerArray);
         });
 
+        // GET QUOTES API
         app.get('/quotes', async (req, res) => {
             const cursor = quotesCollection.find({});
             const quotesArray = await cursor.toArray();
             res.json(quotesArray);
         });
 
+        // GET COVERAGE AREA API
         app.get('/coverage-area', async (req, res) => {
             const cursor = coverageCollection.find({});
             const coverageArray = await cursor.toArray();
             res.json(coverageArray);
         });
 
+        // GET TRIP-TYPES API
         app.get('/trip-types', async (req, res) => {
             const cursor = tripTypesCollection.find({});
             const coverageArray = await cursor.toArray();
             res.json(coverageArray);
         });
 
-        // SINGLE POST API
+        // SINGLE POST API FOR TRIP-TYPES
         app.get('/trip-types/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -66,21 +66,21 @@ async function europeTour() {
             res.send(singleTrip);
         });
 
-        // POST TRIP
+        // POST API FOR TRIP-TYPES
         app.post('/trip-types', async (req, res) => {
             const newUser = req.body;
             const result = await tripTypesCollection.insertOne(newUser);
             res.json(result)
         });
 
-        // GET TRIP API
+        // GET TRIPS API
         app.get('/trips', async (req, res) => {
             const cursor = tripCollection.find({});
             const tripArray = await cursor.toArray();
             res.send(tripArray);
         });
 
-        // ADD TRIP API
+        // ADD API FOR TRIP
         app.post('/trips', async (req, res) => {
             const trip = req.body;
             const result = await tripCollection.insertOne(trip);
@@ -95,7 +95,7 @@ async function europeTour() {
             res.send(findStatus);
         });
 
-        // UPDATE API
+        // UPDATE TRIPS API
         app.put('/trips/:id', async (req, res) => {
             const id = req.params.id;
             const updatedStatus = req.body;
@@ -103,32 +103,32 @@ async function europeTour() {
             const updateDoc = {
                 $set: {
                     status: updatedStatus.status,
+                    tripName: updatedStatus.tripName
                 },
             };
             const result = await tripCollection.updateOne(filter, updateDoc)
             res.send(result)
         });
 
-        // DELETE API
+        // DELETE API FOR TRIPS
         app.delete('/trips/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await tripCollection.deleteOne(query);
             res.json(result);
         })
-
-
-
     } finally {
-        // await client.close();
+        // await client.close();  
     }
 }
 europeTour().catch(console.dir);
 
+// ENSURE SERVER RUNNING
 app.get('/', (req, res) => {
     res.send('Server running for Europe Tour')
 });
 
+// WHICH PORT SERVER ON
 app.listen(port, () => {
     console.log('server running for Europe Tour port ', port)
 });
